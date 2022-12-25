f3d2b92499b3bd3592b7f4a852044f67f8f6958a
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-12-25 15:11:46 +0200

    steps up to 5

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const {
   removeContact,
   addContact,
 } = require("./contacts.js");
+const { Command } = require("commander");
 
 async function doSomeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      await listContacts();
+      const list = await listContacts();
+      console.table(list);
       break;
 
     case "add":
@@ -19,7 +21,7 @@ async function doSomeAction({ action, id, name, email, phone }) {
       await removeContact(id);
       break;
 
-    case "find":
+    case "get":
       await getContactById(id);
       break;
 
@@ -39,4 +41,44 @@ async function doSomeAction({ action, id, name, email, phone }) {
 
 // doSomeAction({ action: "remove", id: "9P_a0OfPSU700VBWzsoOX" });
 
-// doSomeAction({ action: "find", id: "9" });
+// doSomeAction({ action: "get", id: "9" });
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      doSomeAction({ action });
+      break;
+
+    case "add":
+      const [, , , , name, email, phone] = process.argv;
+      doSomeAction({ action, name, email, phone });
+      break;
+
+    case "remove":
+      // const [, , , , , id] = process.argv;
+      doSomeAction({ action, id });
+      break;
+
+    case "get":
+      const [, , , , , id] = process.argv;
+      doSomeAction({ action, id });
+      break;
+
+    default:
+      throw new Error(`unknown action, got: ${action}`);
+  }
+}
+
+invokeAction(argv);
