e2f1ec88448bd0501e6ffb02277d1c4441adf9d3
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-12-25 13:39:02 +0200

    steps 2 & 3

--- contacts.js
+++ contacts.js
@@ -1,1 +1,74 @@
-const fs = require("fs").promises;
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(
+  __dirname,
+  path.dirname("/db/contacts.json"),
+  path.basename("contacts.json")
+);
+
+async function listContacts() {
+  try {
+    const rawData = await fs.readFile(contactsPath, "utf8");
+    const contactList = JSON.parse(rawData);
+    return contactList;
+  } catch (error) {
+    return console.log(error.message);
+  }
+}
+
+async function writeList(data) {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  } catch (error) {
+    return console.log(error.message);
+  }
+}
+
+async function getContactById(id) {
+  const contactList = await listContacts();
+
+  for (const contact of contactList) {
+    if (contact.id === id) {
+      console.log("name: ", contact.name);
+      console.log("email: ", contact.email);
+      console.log("phone: ", contact.phone);
+    }
+  }
+}
+
+async function removeContact(id) {
+  const contactList = await listContacts();
+
+  const updList = contactList.filter((contact) => contact.id !== id);
+
+  await writeList(updList);
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const id = nanoid();
+    const contact = {
+      id,
+      name,
+      email,
+      phone,
+    };
+
+    const contactList = await listContacts();
+
+    contactList.push(contact);
+
+    await writeList(contactList);
+  } catch (error) {
+    return console.log(error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("qwe qwe");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
+
+async function doSomeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      await listContacts();
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      break;
+
+    case "find":
+      await getContactById(id);
+      break;
+
+    default:
+      throw new Error(`unknown action, got: ${action}`);
+  }
+}
+
+// doSomeAction({ action: "list" });
+
+// doSomeAction({
+//   action: "add",
+//   name: "qwe",
+//   email: "qwe@gmail.com",
+//   phone: "123123123",
+// });
+
+// doSomeAction({ action: "remove", id: "9P_a0OfPSU700VBWzsoOX" });
+
+// doSomeAction({ action: "find", id: "9" });
